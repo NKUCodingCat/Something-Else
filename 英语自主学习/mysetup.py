abfbf585bf6da99bf013dfcf26462d59f70228d5
NKUCodingCat <chenjingtao930@gmail.com>
2014-10-27 00:51:06 +0800

    20141027
    英语自主学习

---
+++ 英语自主学习/mysetup.py
@@ -0,0 +1,6 @@
+#-*-coding=utf-8-*-
+# mysetup.py
+from distutils.core import setup
+import py2exe
+
+setup(windows=["N:\\Something\\Something-Else\\英语自主学习\\看答案板-py.py".decode("UTF-8").encode("GBK")])
